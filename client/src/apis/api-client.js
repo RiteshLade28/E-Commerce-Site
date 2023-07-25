780e374df1968846585e59e05ad60a9a98ebbfdd
RiteshLade28 <riteshlade10@gmail.com>
2023-07-25 10:48:05 +0530

    hardcoded the backend url

--- client/src/apis/api-client.js
+++ client/src/apis/api-client.js
@@ -4,7 +4,7 @@ const BASEURL = process.env.BASEURL;
 console.log("BASEURL", BASEURL);
 
 const apiClient = axios.create({
-  baseURL: `${BASEURL}/api`,
+  baseURL: `https://mern-store-backend1.onrender.com/api`,
   headers: {
     "Content-Type": "application/json",
   },
