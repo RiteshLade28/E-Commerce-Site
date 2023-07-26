fe01e498bb785c5ed9ef37ffe53a85ec14b10061
RiteshLade28 <riteshlade10@gmail.com>
2023-07-27 01:18:37 +0530

    change base url to React_app_api_rul

--- client/src/apis/api-client.js
+++ client/src/apis/api-client.js
@@ -1,9 +1,7 @@
 import axios from "axios";
 
-
-
 const apiClient = axios.create({
-  baseURL: `${process.env.git}/api`,
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
