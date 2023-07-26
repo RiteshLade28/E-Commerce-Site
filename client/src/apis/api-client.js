53b2fdb5e79645855a45bb87d79c617e25fb9de8
RiteshLade28 <riteshlade10@gmail.com>
2023-07-27 00:48:17 +0530

    update api-client for baseurl

--- client/src/apis/api-client.js
+++ client/src/apis/api-client.js
@@ -4,7 +4,7 @@ const BASEURL = process.env.BASEURL;
 console.log("BASEURL", BASEURL);
 
 const apiClient = axios.create({
-  baseURL: `http://localhost:5000/api`,
+  baseURL: `${BASEURL}/api`,
   headers: {
     "Content-Type": "application/json",
   },
